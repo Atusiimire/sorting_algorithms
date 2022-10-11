ef2ef80766085b45f7a6ec2483cc0fc572cd5894
Atusiimire <mutatinamoureen@gmail.com>
2022-10-11 13:15:21 -0700

    first commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,20 +9,20 @@
 void insertion_sort_list(listint_t **list)
 {
 listint_t *temp;
-if(list == NULL || *list == NULL)
+if (list == NULL || *list == NULL)
 return;
 temp = (*list)->next;
-while(temp)
+while (temp)
 {
-while(temp->prev && (temp->n < temp->prev->n))
+while (temp->prev && (temp->n < temp->prev->n))
 {
 temp->prev->next = temp->next;
-if(temp->next)
-temp->next->prev =temp->prev;
+if (temp->next)
+temp->next->prev = temp->prev;
 temp->next = temp->prev;
 temp->prev = temp->prev->prev;
 temp->next->prev = temp;
-if(temp->prev == NULL)
+if (temp->prev == NULL)
 *list = temp;
 else
 temp->prev->next = temp;
